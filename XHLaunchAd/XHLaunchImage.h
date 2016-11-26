1a22e3f438e56c0834a34d0bd67798bc5133b932
Xiaohui Zhu <977950862@qq.com>
2016-11-26 08:54:24 +0800

    v2.2.2

--- XHLaunchAd/XHLaunchImage.h
+++ XHLaunchAd/XHLaunchImage.h
@@ -2,9 +2,10 @@
 //  XHLaunchImage.h
 //  XHLaunchAdExample
 //
-//  Created by zhuxiaohui on 2016/11/26.
+//  Created by xiaohui on 16/6/13.
 //  Copyright © 2016年 CoderZhuXH. All rights reserved.
-//
+//  代码地址:https://github.com/CoderZhuXH/XHLaunchAd
+
 
 #import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
