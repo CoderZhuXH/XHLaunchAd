304adae3d8e7336108547ace0e2a777fb4931ee6
Xiaohui Zhu <977950862@qq.com>
2017-04-27 00:45:26 +0800

    v3.4.5

--- XHLaunchAd/XHLaunchAdDownloader.h
+++ XHLaunchAd/XHLaunchAdDownloader.h
@@ -8,33 +8,7 @@
 
 #import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
-
-#pragma mark - XHLaunchAdDownload
-typedef void(^XHLaunchAdDownloadProgressBlock)(unsigned long long total, unsigned long long current);
-
-typedef void(^XHLaunchAdDownloadImageCompletedBlock)(UIImage *_Nullable image, NSData * _Nullable data, NSError * _Nullable error);
-
-typedef void(^XHLaunchAdDownloadVideoCompletedBlock)(NSURL * _Nullable location, NSError * _Nullable error);
-
-@protocol XHLaunchAdDownloadDelegate <NSObject>
-
-- (void)downloadFinishWithURL:(nonnull NSURL *)url;
-
-@end
-
-@interface XHLaunchAdDownload : NSObject
-
-@property (assign, nonatomic ,nonnull)id<XHLaunchAdDownloadDelegate> delegate;
-
-@end
-
-@interface XHLaunchAdImageDownload : XHLaunchAdDownload
-
-@end
-
-@interface XHLaunchAdVideoDownload : XHLaunchAdDownload
-
-@end
+#import "XHLaunchAdDownload.h"
 
 #pragma mark - XHLaunchAdDownloader
 @interface XHLaunchAdDownloader : NSObject
